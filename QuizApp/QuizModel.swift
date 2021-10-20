5fcc7d8679c9877098dcac63a442bd2701749052
Michael Shustov <shustovmm@gmail.com>
2021-10-20 11:56:39 +0300

    - add question data json file to the bundle - add getLocalJsonFile method - add table view to the storyboard - add tableView and questionLabel IBOutlet properties to the ViewController - make ViewController inherit UITableViewDataSource, UITableViewDelegate protocols and add delegate methods

--- QuizApp/QuizModel.swift
+++ QuizApp/QuizModel.swift
@@ -18,9 +18,45 @@ class QuizModel {
     
     func getQuestions() {
         
-        // TODO: Fetch the questions
+        // Fetch the questions
+        getLocalJsonFile()
+    }
+    
+    func getLocalJsonFile() {
         
-        // Notify the delegate of the retrieved questions
-        delegate?.questionsRetrieved([Question]())
+        // Get bundle path for json file
+        let bundlePath = Bundle.main.path(forResource: "QuestionData", ofType: "json")
+        
+        // Double check that path isn't nil
+        guard bundlePath != nil else {
+            print("Couldn't find the JSON data file")
+            return
+        }
+        
+        // Create URL object from the path
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        // Get the data from the url
+        do {
+            
+            let data = try Data(contentsOf: url)
+            
+            // Decode the data
+            let decoder = JSONDecoder()
+            
+            let questionData = try decoder.decode([Question].self, from: data)
+            
+            // Notify the delegate of the retrieved questions
+            delegate?.questionsRetrieved(questionData)
+            
+        }
+        catch {
+            print("Couldn't convert url into data")
+            print(error.localizedDescription)
+        }
+    }
+    
+    func getRemoteJsonFile() {
+        
     }
 }
--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, QuizProtocol {
+class ViewController: UIViewController, QuizProtocol, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     let model = QuizModel()
     var questions = [Question]()
     var currentQuestionIndex = 0
@@ -16,14 +20,53 @@ class ViewController: UIViewController, QuizProtocol {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set self as the data source and delegate of the table view
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // Set up the model
         model.delegate = self
         model.getQuestions()
     }
     
     // MARK: - QuizProtocol Methods
+    
     func questionsRetrieved(_ questions: [Question]) {
         
-        print("Questions retrieved from model!")
+        // Get a reference to the questions
+        self.questions = questions
+        
+        // Reload the table view
+        tableView.reloadData()
+    }
+    
+    // MARK: - UITableView Delegate Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // Make sure that questions array contains a question
+        guard questions.count > 0  else {
+            return 0
+        }
+        
+        // Return the number of questions
+        return questions[currentQuestionIndex].answers?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Get a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnswerCell", for: indexPath)
+        
+        // Customize cell
+        let label = cell.viewWithTag(1) as? UILabel
+        
+        if label != nil {
+            
+            // TODO: Set the answer text for the label
+        }
+        // Return cell
+        return cell
     }
 }
 
